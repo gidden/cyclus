4a8d8a82ba03161cf2ad26f34c1363967b2fa2a3
Matthew Gidden <matthew.gidden@gmail.com>
2015-07-01 10:43:12 -0500

    added empty exchange check and manager now does not try to solve empty exchanges

--- src/resource_exchange.h
+++ src/resource_exchange.h
@@ -117,6 +117,12 @@ class ResourceExchange {
             this));
   }
 
+  /// check if this is an empty exchange (i.e., no requests exist, therefore no
+  /// bids)
+  bool Empty() {
+    return ex_ctx_.bids_by_request.empty();
+  }
+
  private:
   /// @brief queries a given facility agent for
   void AddRequests_(Trader* t) {
